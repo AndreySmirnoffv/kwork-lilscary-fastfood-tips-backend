4e1f30d51508d8371c3a8b436753d25a4756af8f
Andrey Smirnoff <149796739+AndreySmirnoffv@users.noreply.github.com>
2025-01-15 13:53:20 +0100

    updated tsconfig and protection

--- index.ts
+++ index.ts
@@ -4,14 +4,15 @@ import bodyParser from 'body-parser';
 import cors from 'cors'
 import AppRoutes from './src/routes/app.routes'
 import cookieParser from 'cookie-parser'
+import csrf from 'csurf'
 
 dotenv.config({ path: "./.env" });
 
 export const app: Application = express();
+export const csrfProtection = csrf({cookie: true})
 
 app.use(bodyParser.json({limit: '25mb'}));
 app.use(bodyParser.urlencoded({limit: '25mb', extended: true}));
-
 app.use(cookieParser())
 
 app.use(cors({
