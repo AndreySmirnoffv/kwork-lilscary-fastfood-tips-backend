cee060f37e8dd65a41c3f858bb31183442e4b2f9
Andrey Smirnoff <149796739+AndreySmirnoffv@users.noreply.github.com>
2025-01-15 13:43:34 +0100

    removed unused module for production

--- index.ts
+++ index.ts
@@ -4,12 +4,10 @@ import bodyParser from 'body-parser';
 import cors from 'cors'
 import AppRoutes from './src/routes/app.routes'
 import cookieParser from 'cookie-parser'
-import csrf from 'csurf'
 
 dotenv.config({ path: "./.env" });
 
 export const app: Application = express();
-export const csrfProtection = csrf({cookie: true})
 
 app.use(bodyParser.json({limit: '25mb'}));
 app.use(bodyParser.urlencoded({limit: '25mb', extended: true}));
