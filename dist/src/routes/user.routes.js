73aaa100d22997e15568e142611169bfd8db33f1
Andrey Smirnoff <149796739+AndreySmirnoffv@users.noreply.github.com>
2025-01-16 16:52:26 +0100

    Delete dist directory

--- dist/src/routes/user.routes.js
+++
@@ -1,6 +0,0 @@
-import { changePassword, changeUserData, getUser } from "../controllers/user.controller.js";
-import { router } from "./router.js";
-router.patch("/change-userdata", changeUserData);
-router.patch("/change-userpassword", changePassword);
-router.post("/get-user", getUser);
-export default router;
