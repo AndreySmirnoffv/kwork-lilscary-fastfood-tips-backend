323ccf1e58ccfdce2e84f8256aab3ffac82effd8
Andrey Smirnoff <smirnoffa675@gmail.com>
2025-01-16 16:49:45 +0100

    production commit

--- src/utils/utils.hash.ts
+++ src/utils/utils.hash.ts
@@ -1,11 +1,14 @@
 import * as bcrypt from 'bcrypt';
+import dotenv from 'dotenv'
 
-export const hashPassword = async (password: string): Promise<string> => {
-  const salt = await bcrypt.genSalt(10);
+dotenv.config()
+
+export async function hashPassword (password: string): Promise<string> {
+  const salt = await bcrypt.genSalt(Number(process.env.PASSWORD_SALT));
   return await bcrypt.hash(password, salt);
 };
 
-export async function comparePasswords(plainPassword: string, hashedPassword: string): Promise<boolean> {
+export async function comparePasswords (plainPassword: string, hashedPassword: string): Promise<boolean> {
   if (!plainPassword || !hashedPassword) {
     throw new Error("Пароль и хеш обязательны для сравнения");
   }
