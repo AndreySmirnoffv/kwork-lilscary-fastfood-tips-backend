b2f2d483cd6753c176d2a9947f4c01b33629f483
Andrey Smirnoff <smirnoffa675@gmail.com>
2025-01-26 13:29:52 +0100

    added code destroy after 5 min

--- src/controllers/email.conrtoller.ts
+++ src/controllers/email.conrtoller.ts
@@ -1,31 +1,44 @@
 import { Request, Response } from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { transport } from "../services/services.email";
 import { CodeModel } from "../models/code.model";
 
-dotenv.config()
+dotenv.config();
 
-export async function sendEmail(email: string, res: Response){
-    const code = String(Math.floor(Math.random() * 9000) + 1000)
+export async function sendEmail(email: string) {
+    const code = String(Math.floor(Math.random() * 9000) + 1000);
 
-    const response = await transport.sendMail({
-        from: process.env.EMAIL_USER,
-        to: email,
-        subject: "test",
-        text: code
-    }).then(response => console.log(response)).catch(error => console.error(error))
+    try {
+        const response = await transport.sendMail({
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: "test",
+            text: code,
+        });
 
-    transport.verify((error, success) => {
-        if(error){
-            console.error("Error:", error)
-        }else{
-            console.log("server is ready to take our messages:", success)
-        }
-    })
-    CodeModel.createCode(email, code)
+        console.log("Email sent:", response);
 
-    console.log(response)
-    return response
+        transport.verify((error, success) => {
+            if (error) {
+                console.error("Error:", error);
+            } else {
+                console.log("Server is ready to take our messages:", success);
+            }
+        });
+
+        await CodeModel.createCode(email, code);
+
+        setTimeout(() => {
+            CodeModel.destroyCode(code).then(() => console.log("Код был успешно удален")).catch((err) => {
+                console.error(`Ошибка при удалении кода ${code}:`, err);
+            });
+        }, 5 * 60 * 1000);
+
+        return response;
+    } catch (error) {
+        console.error("Ошибка при отправке email:", error);
+        throw error;
+    }
 }
 
 export async function verifyCode(req: Request, res: Response): Promise<any> {
@@ -33,16 +46,15 @@ export async function verifyCode(req: Request, res: Response): Promise<any> {
 
     try {
         const codeExists = await CodeModel.findCode(code);
-        
+
         if (codeExists) {
-            CodeModel.destroyCode(code)
-            return res.json({auth: true});
+            await CodeModel.destroyCode(code);
+            return res.json({ auth: true });
         } else {
             return res.status(500).json({ message: "Такого кода нету" });
         }
     } catch (error) {
-        console.error('Ошибка проверки кода', error);
-        return res.status(500).json({ message: 'Ошибка сервера' });
+        console.error("Ошибка проверки кода", error);
+        return res.status(500).json({ message: "Ошибка сервера" });
     }
 }
-
